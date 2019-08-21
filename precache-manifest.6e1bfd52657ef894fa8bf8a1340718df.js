1797e912ad076600805fe79f48c92ff76db884e3
Evgeniy Studitskih <estrueall@gmail.com>
2019-08-21 15:43:09 +0700

    Updates

---
+++ precache-manifest.6e1bfd52657ef894fa8bf8a1340718df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f54af195c23fb472a064109d8a5e3f",
+    "url": "/campaign-list/index.html"
+  },
+  {
+    "revision": "3611e20514fa5ffe744e",
+    "url": "/campaign-list/static/css/main.685dea8f.chunk.css"
+  },
+  {
+    "revision": "879ad446a85b162af636",
+    "url": "/campaign-list/static/js/2.377625f2.chunk.js"
+  },
+  {
+    "revision": "3611e20514fa5ffe744e",
+    "url": "/campaign-list/static/js/main.e3161cd4.chunk.js"
+  },
+  {
+    "revision": "e443fd9243fb2be47956",
+    "url": "/campaign-list/static/js/runtime~main.71866778.js"
+  }
+]);
